a1572eb8f8b5d29beea5b643f83bdc8102d5fef2
Baljinder Benipal <baljinder.benipal@outlook.com>
2017-01-17 00:16:03 -0500

    Add return button to activity_game.xml

--- app/src/main/java/com/example/abhinavbora/laserclash/Game.java
+++ app/src/main/java/com/example/abhinavbora/laserclash/Game.java
@@ -1,7 +1,10 @@
 package com.example.abhinavbora.laserclash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Game extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Button advanceToScreen1 = (Button) findViewById(R.id.switch_Screen1);
+        advanceToScreen1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Game.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
